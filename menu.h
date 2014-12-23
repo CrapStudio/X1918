7e388fab956508865ac0fd92a0090d0b36d91888
CrapStudio <lordofmacius@gmail.com>
2014-12-23 11:09:43 +0100

    Create menu.h

---
+++ menu.h
@@ -0,0 +1,17 @@
+#ifndef MENU_H
+#define MENU_H
+#include <iostream>
+#include <curses.h>
+#include <windows.h>
+using namespace std;
+
+class menu
+{
+    public:
+        menu();
+        virtual ~menu();
+    protected:
+    private:
+};
+
+#endif // MENU_H
