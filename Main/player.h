ccf1e33d2861c59c171f8919c28577431d0d1dc1
MatthiasPL <jesteskozak@gmail.com>
2014-12-27 21:04:28 +0100

    Update player.h

--- Main/player.h
+++ Main/player.h
@@ -10,10 +10,22 @@ class player
         int hp;
         int pkt_akcji;
         int waga;
+        int inteligencja;
     protected:
     private:
         int base_hp=(3+sila)*4;
         int base_dodge=agl*2;
+
+    player(string nazwa, int sila, int agl, int hp, int pkt_akcji, int waga, int inteligencja, int base_hp, int base_dodge)
+    {
+        this->sila=sila;
+        this->agl=agl;
+        this->hp=hp;
+        this->pkt_akcji=pkt_akcji;
+        this->waga=waga;
+        this->inteligencja=inteligencja;
+        this->base_hp=base_hp;
+    }
 
 
 };
