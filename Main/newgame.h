1c432dba7bf7d7ca8772119723b01887eac35ad4
MatthiasPL <jesteskozak@gmail.com>
2014-12-27 22:18:07 +0100

    Create newgame.h

---
+++ Main/newgame.h
@@ -0,0 +1,17 @@
+#ifndef NEWGAME_H
+#define NEWGAME_H
+#include <iostream>
+#include <curses.h>
+#include <windows.h>
+using namespace std;
+
+class newgame
+{
+    public:
+        newgame();
+        virtual ~newgame();
+    protected:
+    private:
+};
+
+#endif // NEWGAME_H
