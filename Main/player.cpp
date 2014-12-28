7573d374e06c3563db6cec28f19332b0fae64c0b
MatthiasPL <jesteskozak@gmail.com>
2014-12-28 16:49:56 +0100

    Create player.cpp

---
+++ Main/player.cpp
@@ -0,0 +1,55 @@
+#include "player.h"
+
+    string Player::getimie(string imie)
+    {
+        imie=imie;
+        return imie;
+    }
+
+    int Player::getsila(int sila)
+    {
+        sila=sila;
+        return sila;
+    }
+
+    int Player::getagl(int agl)
+    {
+        agl=agl;
+        return agl;
+    }
+
+    int Player::gethp(int hp)
+    {
+        hp=hp;
+        return hp;
+    }
+
+    int Player::getmax_hp(int max_hp)
+    {
+        max_hp=max_hp;
+        return max_hp;
+    }
+
+    int Player::getpkt_akcji(int pkt_akcji)
+    {
+        pkt_akcji=pkt_akcji;
+        return pkt_akcji;
+    }
+
+    int Player::getwaga(int waga)
+    {
+        waga=waga;
+        return waga;
+    }
+
+    int Player::getinteligencja(int inteligencja)
+    {
+        inteligencja=inteligencja;
+        return inteligencja;
+    }
+
+    int Player::getbase_dodge(int base_dodge)
+    {
+        base_dodge=base_dodge;
+        return base_dodge;
+    }
