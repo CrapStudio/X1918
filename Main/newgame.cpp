f5410ac3280338b809dad637b397041b05200767
MatthiasPL <jesteskozak@gmail.com>
2014-12-28 11:50:01 +0100

    Update newgame.cpp

--- Main/newgame.cpp
+++ Main/newgame.cpp
@@ -1,20 +1,28 @@
 #include "newgame.h"
+#include "player.h"
 
 newgame::newgame()
 {
     unsigned int wybor=0;
-    initscr(); //1
+    initscr();
+    for(;;){
+
     printw( "Witaj! Jak masz na imie?\n" );
-    do{//trzeba naprawic!!!!
+
     char imie[10];
     getstr(imie);
+
     mvprintw(1, 2, "Czy Twoje imie to %s? (T/N)\n", imie);
+
     char wybor1;
     wybor1=getch();
+
     if (wybor1=='T' or wybor1=='t') wybor=1;
         else wybor=0;
+    if (wybor==1) break;
+
+    clear();
     }
-    while(wybor==1);
     getch();
     endwin();
 }
