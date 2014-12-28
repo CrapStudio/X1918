a8198358fbc60bb4e525e204e27d211e525d1268
MatthiasPL <jesteskozak@gmail.com>
2014-12-28 16:50:16 +0100

    Update newgame.cpp

--- Main/newgame.cpp
+++ Main/newgame.cpp
@@ -1,31 +1,37 @@
 #include "newgame.h"
 #include "player.h"
+#include <iostream>
 
 newgame::newgame()
 {
-unsigned int wybor=0;
-initscr();
-char imie[10];
-for(;;){
-printw( "Witaj! Jak masz na imie?\n" );
-getstr(imie);
-mvprintw(1, 0, " Czy Twoje imie to %s? (T/N)\n", imie);
-char wybor1;
-wybor1=getch();
-if (wybor1=='T' or wybor1=='t') wybor=1;
-else wybor=0;
-if (wybor==1) break;
-clear();
-}
+    unsigned int wybor=0;
+    initscr();
+    char imie[10];
+    int sila=17;
+    int agl=17;
+    int pkt_akcji=8;
+    int waga=70;
+    int inteligencja=0;
+    char *nazwa[10];
 
-int sila=17;
-int agl=17;
-int pkt_akcji=8;
-int waga=70;
-int inteligencja=0;
+    Player(sila, agl, pkt_akcji, waga, inteligencja);
 
+    for(int i=0;i<10;i++)
+        nazwa[i]=&imie[i];
 
-player(&imie[10],sila,agl,pkt_akcji,waga,inteligencja);
+    for(;;){
+            printw( "Witaj! Jak masz na imie?\n" );
+            getstr(imie);
+            mvprintw(1, 0, " Czy Twoje imie to %s? (T/N)\n", imie);
+            char wybor1;
+            noecho();
+            wybor1=getch();
+            if (wybor1=='T' or wybor1=='t') wybor=1;
+            else wybor=0;
+            if (wybor==1) break;
+            clear();
+            }
+    getch();
 getch();
 endwin();
 }
@@ -33,4 +39,3 @@ newgame::~newgame()
 {
 //dtor
 }
-
