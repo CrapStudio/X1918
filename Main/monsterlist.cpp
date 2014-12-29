8663c30e962c382b33c4f9aa02cfd177669ba5fb
MatthiasPL <jesteskozak@gmail.com>
2014-12-29 14:47:15 +0100

    Create monsterlist.cpp

---
+++ Main/monsterlist.cpp
@@ -0,0 +1,11 @@
+#include "monsterlist.h"
+#include "monsters.h"
+
+Monsterlist::Monsterlist()
+{
+    Monsters wilk;
+    string imie="Wilk";
+    int sila=14;
+    int agl=17;
+    Monsters(imie, sila, agl);
+}
