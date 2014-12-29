ae2fbf8d21374d802c6b75f35a2c521949faf37a
MatthiasPL <jesteskozak@gmail.com>
2014-12-29 13:10:16 +0100

    Create monsters.cpp

---
+++ Main/monsters.cpp
@@ -0,0 +1,55 @@
+#include "monsters.h"
+
+    Monsters::Monsters()
+    {
+    imie="";
+    sila=0;
+    agl=0;
+    hp=0;
+    pkt_akcji=0;
+    waga=0;
+    inteligencja=0;
+    }
+
+    Monsters::Monsters(string imie, int sila, int agl)
+    {
+        imie=imie;
+        sila=sila;
+        agl=agl;
+    }
+
+    string Monsters::getimie(string imie)
+    {
+        imie=imie;
+        return imie;
+    }
+
+    int Monsters::getsila(int sila)
+    {
+        sila=sila;
+        return sila;
+    }
+
+    int Monsters::getagl(int agl)
+    {
+        agl=agl;
+        return agl;
+    }
+
+    int Monsters::gethp(int hp)
+    {
+        hp=hp;
+        return hp;
+    }
+
+    int Monsters::getmax_hp(int max_hp)
+    {
+        max_hp=max_hp;
+        return max_hp;
+    }
+
+    int Monsters::getbase_dodge(int base_dodge)
+    {
+        base_dodge=base_dodge;
+        return base_dodge;
+    }
